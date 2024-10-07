bb38d86b5a0584acbfbe0c59f33b27dbbe2fe67f
Landr0val <github@eridanus.8shield.net>
2024-10-07 12:14:18 -0500

    added: get_alert_config

--- src/routers/alert_router.py
+++ src/routers/alert_router.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Body, APIRouter, Query
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from src.models.reads_model import Read
 from src.models.alert_model import Alert
-from src.models.getAlert_model import getAlert_model
+from src.models.getAlert_model import AlertConfig
 from src.controllers.alert_controller import AlertController
 
 alert_controller = AlertController()
@@ -15,6 +15,6 @@ async def save_alert(alert: Alert):
     return response
 
 @alert_router.get("/get_alert_config")
-def get_alert_config_endpoint(request: getAlert_model = Body(...)):
+def get_alert_config_endpoint(request: AlertConfig = Body(...)):
     response = alert_controller.get_alerts(request.user_id)
     return response

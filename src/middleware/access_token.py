579e25d11d527bf1e77648a035b45d91cfc463e9
Jesus Torrenegra <jdtrorrenegrap@ul.edu.co>
2025-03-17 14:23:00 -0500

    Set default value for ACCESS_TOKEN_EXPIRE to prevent NoneType error

--- src/middleware/access_token.py
+++ src/middleware/access_token.py
@@ -7,18 +7,18 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE= int(os.getenv("ACCESS_TOKEN_EXPIRE"))
+ACCESS_TOKEN_EXPIRE_MINUTES= int(os.getenv("ACCESS_TOKEN_EXPIRE","30"))
 
 if SECRET_KEY is None:
     raise ValueError("No SECRET_KEY set for access token generation")
 if ALGORITHM is None:
     raise ValueError("No ALGORITHM set for access token generation")
-if ACCESS_TOKEN_EXPIRE is None:
-    raise ValueError("No ACCESS_TOKEN_EXPIRE set for access token generation")
+if ACCESS_TOKEN_EXPIRE_MINUTES is None:
+    raise ValueError("No ACCESS_TOKEN_EXPIRE_MINUTES set for access token generation")
     
 
 def create_access_token(user_id: int, username: str, email: str, rol_id: int):
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     payload = {
         "sub": str(user_id),
         "username": username,
